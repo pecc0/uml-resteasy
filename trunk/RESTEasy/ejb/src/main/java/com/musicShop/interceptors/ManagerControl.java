fa00ccde6c794c3368e8bc57a632c0eac83b1ed3
air_biker@abv.bg <air_biker@abv.bg@017d1e60-2a0c-11df-8288-31e8ecb78f5f>
2010-03-08 16:33:59 +0000

    jquery

--- trunk/RESTEasy/ejb/src/main/java/com/musicShop/interceptors/ManagerControl.java
+++ trunk/RESTEasy/ejb/src/main/java/com/musicShop/interceptors/ManagerControl.java
@@ -1,9 +1,10 @@
 package com.musicShop.interceptors;
 
+import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
 public class ManagerControl {
-
+	@AroundInvoke
 	public Object checkManager(InvocationContext aInvocationContext) throws Exception {
 		System.out.println("check the manager");
 		return aInvocationContext.proceed();
